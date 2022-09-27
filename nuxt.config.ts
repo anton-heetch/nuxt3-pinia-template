e870098ff4c83b10634c6ced2a73aa1e7def8cb5
Anton Sokolov <green.frost@gmail.com>
2022-09-27 14:59:33 +0600

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  srcDir: 'src/',
+  alias: {
+    icons: resolve(__dirname, './src/assets/icons'),
+    atoms: resolve(__dirname, './src/components/atoms'),
+    molecules: resolve(__dirname, './src/components/molecules'),
+    organisms: resolve(__dirname, './src/components/organisms'),
+    templates: resolve(__dirname, './src/components/templates'),
+    directives: resolve(__dirname, './src//directives'),
+    services: resolve(__dirname, './src/services'),
+    repositories: resolve(__dirname, './src/repositories'),
+    plugins: resolve(__dirname, './src/plugins'),
+  },
+  css: ['~/assets/scss/main.scss'],
+  buildModules: [],
+  modules: [],
+})
