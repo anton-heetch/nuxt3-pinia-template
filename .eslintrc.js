de9ccd89b80a71e7182eb03a47aea897879765fd
Anton Sokolov <green.frost@gmail.com>
2022-11-18 17:38:45 +0600

    fix: lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
+    es2021: true,
     node: true,
   },
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 'lateset',
     sourceType: 'module',
     parser: '@typescript-eslint/parser',
   },
